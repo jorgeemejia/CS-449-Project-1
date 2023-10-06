9bd084167fe42d43e6d91edf4f08b340dc58da18
hhoang91 <huywow147@gmail.com>
2023-10-05 23:10:29 -0700

    Change db schema from date to datetime.

--- share/droplists.sql
+++ share/droplists.sql
@@ -5,10 +5,10 @@ CREATE TABLE droplists (
     StudentID INTEGER,
     ClassID INTEGER,
     AdminDrop BOOLEAN,
-    DropDate DATE,
+    DropDate DATETIME,
     PRIMARY KEY(StudentID, ClassID)
     CONSTRAINT fk_drop_students FOREIGN KEY (StudentID) REFERENCES students(StudentID)
     CONSTRAINT fk_drop_classes FOREIGN KEY (ClassID) REFERENCES classes(ClassID)
 );
-INSERT INTO droplists(StudentID, ClassID, AdminDrop, DropDate) VALUES (01, 01, 0, CURRENT_DATE);
+INSERT INTO droplists(StudentID, ClassID, AdminDrop, DropDate) VALUES (01, 01, 0, CURRENT_TIMESTAMP);
 COMMIT;
