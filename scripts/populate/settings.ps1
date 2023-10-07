8e51ecdae5d7abcbef5a7f4edd9c64e1e3368dcd
McGabriel99 <mcgabrielf@csu.fullerton.edu>
2023-10-07 01:13:09 -0700

    populate settings table

---
+++ scripts/populate/settings.ps1
@@ -0,0 +1,7 @@
+# Check if the 'var' folder exists, and create it if it doesn't as well as the nested 'log' folder
+if (-not (Test-Path -Path ".\var" -PathType Container)) {
+    New-Item -Path ".\var\log" -ItemType Directory
+}
+
+# Execute settings.sql
+Get-Content .\share\settings.sql | sqlite3 .\var\titanonline_clone.db
